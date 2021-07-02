f446e5b52c8f5a718cdc5b505227ed9d9beb6bfe
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-07-01 20:24:21 -0400

    Completed Lesson 23

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+    [SerializeField] float speedGainPerSecond = 0.2f;
+
+    void Update()
+    {
+        speed += speedGainPerSecond * Time.deltaTime;
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
