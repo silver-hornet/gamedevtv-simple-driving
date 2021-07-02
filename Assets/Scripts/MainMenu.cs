bf8e03a13f00446eeffbf5663ceb160f10291f74
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-07-02 14:55:13 -0400

    Completed Lesson 30

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,19 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] TMP_Text highScoreText;
+    [SerializeField] TMP_Text energyText;
+    [SerializeField] int maxEnergy;
+    [SerializeField] int energyRechargeDuration;
+
+    int energy;
+
+    const string EnergyKey = "Energy";
+    const string EnergyReadyKey = "EnergyReady";
 
     void Start()
     {
         int highScore = PlayerPrefs.GetInt(ScoreSystem.HighScoreKey, 0);
         highScoreText.text = $"High Score: {highScore}";
+        energy = PlayerPrefs.GetInt(EnergyKey, maxEnergy);
+
+        if (energy == 0)
+        {
+            string energyReadyString = PlayerPrefs.GetString(EnergyReadyKey, string.Empty);
+
+            if (energyReadyString == string.Empty) { return; }
+
+            DateTime energyReady = DateTime.Parse(energyReadyString);
+
+            if (DateTime.Now > energyReady)
+            {
+                energy = maxEnergy;
+                PlayerPrefs.SetInt(EnergyKey, energy);
+            }
+        }
+
+        energyText.text = $"Play ({energy})";
     }
 
     public void Play()
     {
+        if (energy < 1) { return; }
+
+        energy--;
+        PlayerPrefs.SetInt(EnergyKey, energy);
+
+        if (energy == 0)
+        {
+            DateTime energyReady = DateTime.Now.AddMinutes(energyRechargeDuration);
+            PlayerPrefs.SetString(EnergyReadyKey, energyReady.ToString());
+        }
+
         SceneManager.LoadScene(1);
     }
 }
